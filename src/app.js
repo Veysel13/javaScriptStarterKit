b5a85ae9c734d91ae71f3bbfaa716dc25cab04ef
Veysel <veysel.akpinar@4alabs.io>
2021-10-19 23:58:16 +0300

    18 ekim ders sonu

---
+++ src/app.js
@@ -0,0 +1,24 @@
+console.log("merhaba veysel")
+
+let dolarDun = 9.20
+let dolarBugun = 9.30
+
+{
+    let dolarDun=9.4
+}
+
+
+console.log(dolarDun)
+
+const euroDun=11.2
+//euroDun=34.5
+console.log(euroDun)
+
+let konutKredileri=["Konut kredisi","Emlak konut kredisi","kamu konut kredisi"]
+console.log("<ul>")
+for (let index = 0; index < konutKredileri.length; index++) {
+    const element = konutKredileri[index];
+    console.log("<li>"+konutKredileri[index]+"</li>")
+}
+console.log("</ul>")
+console.log(konutKredileri)
